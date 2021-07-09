0c848f2908bf8341334e361ec928904b42962878
Herick Freitas <herickfreitas@gmail.com>
2021-07-09 19:15:50 -0300

    Manutenção

--- datasets/SQL/Fluig_Autorizador_IDFLUIG.sql
+++ datasets/SQL/Fluig_Autorizador_IDFLUIG.sql
@@ -1,12 +1,13 @@
-USE [Corporerm]
+USE [corporerm]
 GO
 
-/****** Object:  UserDefinedFunction [dbo].[Fluig_Autorizador_IDFLUIG]    Script Date: 10/05/2021 20:27:37 ******/
+/****** Object:  UserDefinedFunction [dbo].[Fluig_Autorizador_IDFLUIG]    Script Date: 28/06/2021 12:05:17 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
+
 
 
 
@@ -25,7 +26,10 @@ AS
 			/*	Solicitação de Autorização de Fornecimento	*/	
 			SELECT @Retorno = autorizador FROM [RIOSRV013\SISTEMA].[FLUIG_DB].[dbo].[ML001077] WHERE n_solicitacao = @idfluig
 
-		
+		else if ((SELECT count(*) FROM [RIOSRV013\SISTEMA].[FLUIG_DB].[dbo].[TAR_PROCES] where num_proces = @idfluig and NUM_SEQ_ESCOLHID = 140) > 0)
+			/*	Solicitação de Outras Entradas	- Escalar outro aprovador */	
+			SELECT @Retorno = COD_MATR_ESCOLHID FROM [RIOSRV013\SISTEMA].[FLUIG_DB].[dbo].[TAR_PROCES] where num_proces = @idfluig and NUM_SEQ_ESCOLHID = 140
+
 		else if ((SELECT count(*) FROM [RIOSRV013\SISTEMA].[FLUIG_DB].[dbo].[ML001078] WHERE n_solicitacao = @idfluig) > 0)
 			/*	Solicitação de Outras Entradas	*/	
 			SELECT @Retorno = autorizador FROM [RIOSRV013\SISTEMA].[FLUIG_DB].[dbo].[ML001078] WHERE n_solicitacao = @idfluig
